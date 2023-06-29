9855b868e595c014aa81e3cf10aa6a8f0cbd8981
Elena Pilyugina <elena.pilyugina.job@gmail.com>
2023-06-29 17:26:12 +0300

    implement the selection of annotated methods

--- agent/src/main/java/com/github/pielena/Agent.java
+++ agent/src/main/java/com/github/pielena/Agent.java
@@ -1,5 +1,11 @@
 package com.github.pielena;
 
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.tree.AnnotationNode;
+import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.MethodNode;
+
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.Instrumentation;
 import java.security.ProtectionDomain;
@@ -15,10 +21,44 @@ public class Agent {
                                     ProtectionDomain protectionDomain,
                                     byte[] classfileBuffer) {
 
-                System.out.println("Loading class: " + className);
+                if (className.startsWith("com/github/pielena")) {
+                    System.out.println(className);
+
+                    return changeMethod(classfileBuffer);
+                }
 
                 return classfileBuffer;
             }
         });
     }
+
+    private static byte[] changeMethod(byte[] originalClass) {
+        ClassReader cr = new ClassReader(originalClass);
+        ClassWriter cw = new ClassWriter(cr, ClassWriter.COMPUTE_MAXS);
+
+        ClassNode classNode = new ClassNode();
+        cr.accept(classNode, ClassReader.EXPAND_FRAMES);
+
+        for (MethodNode methodNode : classNode.methods) {
+            System.out.println(methodNode.name);
+            boolean hasAnnotation = false;
+            System.out.println("methodNode.invisibleAnnotations: " + methodNode.invisibleAnnotations);
+            if (methodNode.invisibleAnnotations != null) {
+                for (AnnotationNode annotationNode : methodNode.invisibleAnnotations) {
+                    System.out.println(annotationNode.desc);
+                    if (annotationNode.desc.equals("Lcom/github/pielena/Log;")) {
+                        hasAnnotation = true;
+                        System.out.println("Hello, I've found the annotation!");
+                        break;
+                    }
+                }
+            }
+
+            if (hasAnnotation) {
+
+            }
+        }
+        return originalClass;
+    }
+
 }
